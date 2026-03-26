9830a2170a36c7c615d181798be4da22e2a524dd
fish2363 <a01022639410@gmail.com>
2026-03-26 21:27:31 +0900

    Update SequenceCore.cs

--- Code/GameJam_GBGC/Sequence/SequenceCore.cs
+++ Code/GameJam_GBGC/Sequence/SequenceCore.cs
@@ -2,7 +2,6 @@ using Ami.BroAudio;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class SequenceCore : MonoBehaviour
