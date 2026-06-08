b9084d05264dc9fbaa1c923025f9ea5e9ad13230
fish2363 <a01022639410@gmail.com>
2026-06-08 22:37:39 +0900

    fix : UIService

--- Code/Offering/Core/UI/IUIService.cs
+++ Code/Offering/Core/UI/IUIService.cs
@@ -2,7 +2,6 @@ using Cysharp.Threading.Tasks;
 
 public interface IUIService : IGameService
 {
-    public UniTask<T> ShowAsync<T>(params object[] param) where T : UIBase;
     public void Hide<T>(params object[] param) where T : UIBase;
     public T Get<T>() where T : UIBase;
     public bool IsOpened<T>() where T : UIBase;
